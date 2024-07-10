8b26c742bd0d84a9d9cafb638e50467d1e769447
jdjoum <jdjoum@hotmail.com>
2024-07-10 17:10:09 -0400

    Add Input to Form

--- src/components/AddWeightForm.jsx
+++ src/components/AddWeightForm.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { Form } from 'react-router-dom';
 
 const AddWeightForm = () => {
-  return (
+    // const input = document.getElementById("newWeightAmount");
+    // const inputValue = input.value;
+    // console.log(inputValue);
+
+    return (
     <div className="addWeightForm">
         <h2>Add New Weight Entry</h2>
         <Form method="post">
+            <input type="number" step="0.01" name="newWeightAmount" id="newWeightAmount" placeholder='ex. 175 lbs' required inputMode='decimal'/>
             <button type="submit">
                 <span>Add New Weight Entry</span>
             </button>
         </Form>
     </div>
-  )
+    )
 }
 
 export default AddWeightForm
