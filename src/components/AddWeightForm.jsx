16446a2c7b6c6c50cf7a76ee36a7f31436ed330b
jdjoum <jdjoum@hotmail.com>
2024-07-11 14:07:43 -0400

    Retrieve Weight Input Value and Log It

--- src/components/AddWeightForm.jsx
+++ src/components/AddWeightForm.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'react-router-dom';
 
 const AddWeightForm = () => {
-    // const input = document.getElementById("newWeightAmount");
-    // const inputValue = input.value;
-    // console.log(inputValue);
+    const [inputValue, setInputValue] = useState('');
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const inputWeight = { inputValue };
+        console.log(inputWeight);
+    }
 
     return (
     <div className="addWeightForm">
         <h2>Add New Weight Entry</h2>
-        <Form method="post">
-            <input type="number" step="0.01" name="newWeightAmount" id="newWeightAmount" placeholder='ex. 175 lbs' required inputMode='decimal'/>
+        <Form method="post" onSubmit={handleSubmit}>
+            <input type="number" step="0.01" name="newWeightAmount" id="newWeightAmount" placeholder='ex. 175 lbs' required inputMode='decimal' onChange={handleInputChange}/>
             <button type="submit">
                 <span>Add New Weight Entry</span>
             </button>
         </Form>
+        <p>{inputValue}</p>
     </div>
     )
 }
