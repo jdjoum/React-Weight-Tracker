d8d582468fc007f502f372747778f1f30546e7bc
jdjoum <jdjoum@outlook.com>
2024-08-29 16:40:02 -0400

    Additional display mode changes

--- src/actions/updateDisplayMode.js
+++ src/actions/updateDisplayMode.js
@@ -1,15 +1,9 @@
 // RRD Imports
 import { redirect } from "react-router-dom";
 
-// Library Imports
-import { toast } from "react-toastify";
-
-// Helper Functions
-import { deleteItem } from "../helper";
-
 export async function updateDisplayModeAction() {
-    const displayMode = JSON.parse(localStorage.getItem("displayMode"));
-    toast.success(`You updated the display mode to ${displayMode}!`);
+    location.reload();
     // Return redirect
-    return redirect("/React-Weight-Tracker/")
+    redirect("/React-Weight-Tracker/")
+    return null
 }
